248a238c6a84b81d158332602619f3baf812a32e
Tokyo <ahmed.tokyo1@gmail.com>
2017-11-06 03:33:53 +0200

    add move script

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class move : MonoBehaviour {
+	public GameObject player;
+	private float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis ("Horizontal");
+		transform.Translate (new Vector3 (x,0, speed * Time.deltaTime));
+		if (Input.GetKeyDown ("space")){
+			transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+		}
+	}
+}
