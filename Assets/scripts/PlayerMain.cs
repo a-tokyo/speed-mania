50d72988ecb6d8fc8299e68c6c7812c4ccfe6ad4
Tokyo <ahmed.tokyo1@gmail.com>
2017-11-06 07:39:31 +0200

    add basic sounds

--- Assets/scripts/PlayerMain.cs
+++ Assets/scripts/PlayerMain.cs
@@ -7,12 +7,16 @@ public class PlayerMain : MonoBehaviour {
 	public GameObject player;
 	private int score = 0;
 	//	public Text scoreText;
-	private float speed = 1.0f;
+	private float speed = 2.0f;
 	//	public Text speedText;
+	public bool mute = false;
+
+	private AudioSource audio = null;
 
 	// Use this for initialization
 	void Start () {
-
+		audio = GetComponent< AudioSource> ();
+		playSound("super-mario");
 	}
 
 	// Update is called once per frame
@@ -20,19 +24,21 @@ public class PlayerMain : MonoBehaviour {
 		float x = Input.GetAxis ("Horizontal");
 		transform.Translate (new Vector3 (x,0, speed * Time.deltaTime));
 		if (Input.GetKeyDown ("space")){
+			playSound("super-mario");
 			transform.Translate(Vector3.up * 1.2f);
 		}
 	}
 
-	void OnCollisionEnter(Collision c){
-		//		GetComponent< AudioSource> ().Play ();
-		if (c.gameObject.CompareTag ("Coin")) {
-			GameObject.Destroy (c.gameObject);
+	void OnCollisionEnter(Collision collisionObject){
+		if (collisionObject.gameObject.CompareTag ("Coin")) {
+			playSound("super-mario");
+			GameObject.Destroy (collisionObject.gameObject);
 			score += 10;
 			//			scoreText.text = "" + score;
 		}
-		if (c.gameObject.CompareTag ("Radar")) {
-			GameObject.Destroy (c.gameObject);
+		if (collisionObject.gameObject.CompareTag ("Radar")) {
+			playSound("super-mario");
+			GameObject.Destroy (collisionObject.gameObject);
 			if (score - 50 >= 0) {
 				score -= 50;
 			} else {
@@ -40,9 +46,25 @@ public class PlayerMain : MonoBehaviour {
 			}
 			//			scoreText.text = "" + score;
 		}
-		if (c.gameObject.CompareTag ("Obstacle")) {
+		if (collisionObject.gameObject.CompareTag ("Obstacle")) {
+			playSound("super-mario");
 			// @TODO
 		}
 		speed = speed * ((int)(speed / 50) + 1);
+	}
+
+
+	/**
+	 * Plays a sound from the resources folder if the mute is set to false
+	 **/
+	private void playSound(string resourcesPath, bool continious = false){
+		if (mute) {
+			return;
+		}
+		if (continious) {
+			
+		} else {
+			audio.PlayOneShot((AudioClip)Resources.Load (resourcesPath));
+		}
 	}
 }
