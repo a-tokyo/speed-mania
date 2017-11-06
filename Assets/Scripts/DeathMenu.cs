13a60e968cf2a9bd44b2fdb2d0e590656d91dc5e
Tokyo <ahmed.tokyo1@gmail.com>
2017-11-06 22:53:05 +0200

    add basic menu

--- Assets/Scripts/DeathMenu.cs
+++ Assets/Scripts/DeathMenu.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeathMenu : MonoBehaviour {
+	public Text scoreText;
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +18,6 @@ public class DeathMenu : MonoBehaviour {
 
 	public void ToggleEndMenu(int score = 0){
 		gameObject.SetActive (true);
+		scoreText.text = "" + score;
 	}
 }
