33599ac99b1db8fbbd1e628a952693c9977d0567
Tokyo <ahmed.tokyo1@gmail.com>
2017-11-06 23:26:09 +0200

    add stupid extra screens

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject MainMenuCtr;
+
+	public ExtraMenu credits;
+	public ExtraMenu howToPlay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void ToggleCredits() {
+		MainMenuCtr.SetActive (false);
+		credits.Toggle();
+	}
+
+	public void ToggleHowTo() {
+		MainMenuCtr.SetActive (false);
+		howToPlay.Toggle();
+	}
+}
